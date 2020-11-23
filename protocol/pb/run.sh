0fd9426552353aa88555b49ad1ac980f46927b0f
perrynzhou <perrynzhou@gmail.com>
2020-11-23 10:30:06 +0800

    update object design

--- protocol/pb/run.sh
+++ protocol/pb/run.sh
@@ -1,7 +1,9 @@
 rm -rf *.go
 #go grpc
 protoc --go_out=plugins=grpc:. bucket.proto
+protoc --go_out=plugins=grpc:. object.proto
 protoc --go_out=plugins=grpc:. service.proto
 #gateway
 protoc --grpc-gateway_out=logtostderr=true:. bucket.proto
+protoc --grpc-gateway_out=logtostderr=true:. object.proto
 protoc --grpc-gateway_out=logtostderr=true:. service.proto
